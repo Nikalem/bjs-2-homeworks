66d360cc5308a9e730acfc7c6dbcb23886bf277d
Nikalem <62104047+Nikalem@users.noreply.github.com>
2023-06-27 20:31:36 +0300

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.marks === undefined) {
+        return 0;
+    }
+    this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
-  
+    if ((this.marks === undefined) || this.marks.length == 0) {
+        return 0;
+    }
+
+    const sum = this.marks.reduce(function (acc, mark) {
+        return acc + mark;
+    })
+    return sum/this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
