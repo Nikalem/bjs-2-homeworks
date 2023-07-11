c9612aae750b975b2afb8d1ad0271086524bd22e
Nikalem <62104047+Nikalem@users.noreply.github.com>
2023-07-11 14:46:03 +0300

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
 ﻿
+
+function parseCount(num) {
+    const res = Number.parseFloat(num);
+    if(isNaN(res)) {
+        throw new Error("Невалидное значение");
+    }
+    return res
+}
+
+function validateCount(num){
+    try {
+        return parseCount(num);
+    } catch(err) {
+        return err;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        if (((a + b) < c) || ((b + c) < a) || ((c + a) < b)) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    } 
+
+    get area() {
+        const p = this.perimeter/2;
+        const res = Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+        return res;
+    }
+}
+
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get perimeter() {return 'Ошибка! Треугольник не существует'},
+            get area() {return 'Ошибка! Треугольник не существует'}
+        }
+    }
+}
